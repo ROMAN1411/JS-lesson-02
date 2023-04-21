46ed1168ef0ca831a7f6d4d8d939c7544daed53d
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2023-04-21 12:15:53 +0300

    05

---
+++ JS/05-dom.js
@@ -0,0 +1,35 @@
+const btnAdd = document.querySelector("button[data-add]");
+const btnClean = document.querySelector("button[data-reset]");
+const valueInput = document.querySelector("input[data-value]");
+const outputEl = document.querySelector("[data-output] span");
+
+let total = 0;
+
+console.log(btnAdd.textContent);
+console.log(valueInput);
+
+btnAdd.addEventListener("click", function () {
+  console.log("click Add");
+  console.log(valueInput.value);
+  console.log(typeof valueInput.value);
+  console.log(typeof Number(valueInput.value));
+  console.log(Number(valueInput.value));
+
+  const value = Number(valueInput.value);
+
+  total += value;
+
+  outputEl.textContent = total;
+
+  console.log("total", total);
+
+  valueInput.value = 0;
+
+  console.log("--------------------------------------");
+});
+
+btnClean.addEventListener("click", function () {
+  total = 0;
+  outputEl.textContent = total;
+  console.log("total", total);
+});
